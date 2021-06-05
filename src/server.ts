7826151c510f5939a378da1f3db14e10531dfc60
r0cc4t0 <guilherme.roccato@outlook.com>
2021-06-05 17:45:29 -0300

    Concepts: Routes, Entities, Repositories, and Controllers

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
 import express from 'express';
+import 'reflect-metadata';
 import './database';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello, World!' });
-});
-
-app.post('/', (request, response) => {
-  return response.json({ message: 'Data has been saved successfully!' });
-});
+app.use(routes);
 
 app.listen(3333, () => console.log('Server is running on port 3333...'));
