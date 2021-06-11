285b9b87b1d30a97e475d879f967e4a2640a9be3
r0cc4t0 <guilherme.roccato@outlook.com>
2021-06-10 22:11:01 -0300

    Socket.io Configuration

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 import 'reflect-metadata';
 import './database';
 import routes from './routes';
@@ -9,4 +11,12 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => console.log('Server is running on port 3333...'));
+const http = createServer(app);
+
+const io = new Server(http);
+
+io.on('connection', (socket: Socket) => {
+  console.log('Successfully connected!', socket.id);
+});
+
+http.listen(3333, () => console.log('Server is running on port 3333...'));
