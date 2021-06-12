d3036dbf642a009f4c079b9ddfac65522ed75e94
r0cc4t0 <guilherme.roccato@outlook.com>
2021-06-11 22:50:58 -0300

    Static Files

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,23 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server, Socket } from 'socket.io';
+import path from 'path';
 import 'reflect-metadata';
 import './database';
 import routes from './routes';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set('views', path.join(__dirname, '..', 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 app.use(express.json());
-
 app.use(routes);
+
+app.get('/pages/client', (request, response) => {
+  return response.render('html/client.html');
+});
 
 const http = createServer(app);
 
