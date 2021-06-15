36057405dd9747ad1e4f38abf18a02951380edd0
r0cc4t0 <guilherme.roccato@outlook.com>
2021-06-14 23:58:13 -0300

    Client Connection

---
+++ src/services/ConnectionsService.ts
@@ -0,0 +1,35 @@
+import { getCustomRepository, Repository } from 'typeorm';
+import Connection from '../entities/Connection';
+import ConnectionsRepository from '../repositories/ConnectionsRepository';
+
+interface Connections {
+  id?: string;
+  admin_id?: string;
+  user_id: string;
+  socket_id: string;
+}
+
+class ConnectionsService {
+
+  private connectionsRepository: Repository<Connection>;
+
+  constructor() {
+    this.connectionsRepository = getCustomRepository(ConnectionsRepository);
+  }
+
+  async create({ id, admin_id, user_id, socket_id }: Connections) {
+    const connection = this.connectionsRepository.create({ id, admin_id, user_id, socket_id });
+
+    await this.connectionsRepository.save(connection);
+
+    return connection;
+  }
+
+  async findByUserId(user_id: string) {
+    const connection = await this.connectionsRepository.findOne({ user_id });
+    return connection;
+  }
+
+}
+
+export default ConnectionsService;
--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,7 +1,43 @@
 import { io } from '../http';
+import ConnectionsService from '../services/ConnectionsService';
+import UsersService from '../services/UsersService';
+import MessagesService from '../services/MessagesService';
 
 io.on('connection', socket => {
-  socket.on('client_first_access', params => {
-    console.log(params);
+  const connectionsService = new ConnectionsService();
+  const usersService = new UsersService();
+  const messagesService = new MessagesService();
+
+  socket.on('client_first_access', async (params) => {
+    const socket_id = socket.id;
+    const { text, email } = params;
+
+    let user_id = null;
+    const userAlreadyExists = await usersService.findByEmail(email);
+
+    if (!userAlreadyExists) {
+      const user = await usersService.create(email);
+
+      await connectionsService.create({ socket_id, user_id: user.id });
+
+      user_id = user.id;
+    }
+    else {
+      user_id = userAlreadyExists.id;
+
+      const connection = await connectionsService.findByUserId(userAlreadyExists.id);
+
+      if (!connection) {
+        await connectionsService.create({ socket_id, user_id: userAlreadyExists.id });
+      }
+      else {
+        connection.socket_id = socket_id;
+        await connectionsService.create(connection);
+      }
+
+      await connectionsService.create({ socket_id, user_id: userAlreadyExists.id });
+    }
+
+    await messagesService.create({ text, user_id });
   });
 });
